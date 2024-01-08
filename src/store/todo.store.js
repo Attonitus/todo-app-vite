b32c461df990ff34e756fdd184033d29a3038f24
Attonitus <xdkakaxd123@gmail.com>
2024-01-08 12:13:04 -0600

    State methods implement

--- src/store/todo.store.js
+++ src/store/todo.store.js
@@ -25,31 +25,58 @@ const loadStore = () => {
 
 }
 
-const addTodo = ( description ) => {
+const getTodos = (filter = filters.All) => {
+    switch(filter){
+        case filters.All:
+            return [...state.todos];
+        case filters.Completed:
+            return state.todos.filter( todo => todo.done);
+        case filters.Pending:
+            return state.todos.filter( todo => !todo.done);
+        default:
+            throw new Error(`Option ${filter} not valid!`)
+    }
+}
 
+const addTodo = ( description ) => {
+    if(!description) throw new Error("Description is required");
+    state.todos.push( new Todo(description) );
 }
 
 const toggleTodo = (todoId) => {
-
+    state.todos = state.todos.map( todo => {
+        if(todo.id === todoId){
+            todo.done = !todo.done;
+        }
+        return todo;
+    });
 }
 
 const deleteTodo = (todoId) => {
-    
+    if(!todoId) throw new Error("Id is required!");
+    state.todos = state.todos.filter( todo => todo.id !== todoId);
 }
 
 const deleteCompleted = () => {
-    
+    state.todos = state.todos.filter( todo => todo.done);
 }
 
-
 const setFilter = ( newFilter = filters.All ) => {
-
+    state.filter = newFilter;
 }
 
 const getCurrentFilter = () => {
-    
+    return state.filter;
 }
 
 export default {
     initStore,
+    getTodos,
+    addTodo,
+    loadStore,
+    toggleTodo,
+    deleteTodo,
+    deleteCompleted,
+    setFilter,
+    getCurrentFilter
 }
