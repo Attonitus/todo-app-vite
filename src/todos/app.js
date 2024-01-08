fdc0ecb5a3bd7d27269373b29397e82e123e685d
Attonitus <xdkakaxd123@gmail.com>
2024-01-08 12:50:50 -0600

    render todos

--- src/todos/app.js
+++ src/todos/app.js
@@ -1,4 +1,10 @@
+import todoStore from '../store/todo.store';
 import html from './app.html?raw';
+import { renderTodos } from './use-cases';
+
+const ElementsIds = {
+    TodoList: '.todo-list',
+}
 
 /**
  * 
@@ -7,9 +13,14 @@ import html from './app.html?raw';
 
 export const App = (elementID) => {
 
+    const displayTodos = () => {
+        const todos = todoStore.getTodos( todoStore.getCurrentFilter());
+        renderTodos(ElementsIds.TodoList, todos)
+    }
     (()=> {
         const app = document.createElement('div');
         app.innerHTML= html;
         document.querySelector(elementID).append(app);
+        displayTodos();
     })();
 }
---
+++ src/todos/use-cases/render-todos.js
@@ -0,0 +1,20 @@
+import { Todo } from "../models/todo.models";
+import { createTodoHTML } from "./create.todo.html";
+
+let element;
+
+/**
+ * 
+ * @param {String} elementID 
+ * @param {Todo} todos 
+ */
+export const renderTodos = (elementID, todos = []) => {
+    if(!element) element = document.querySelector(elementID);
+    if(!element) throw new Error(`${elementID} not found`);
+
+    element.innerHTML = '';
+
+    todos.forEach(todo => {
+        element.append(createTodoHTML(todo));
+    });
+}
